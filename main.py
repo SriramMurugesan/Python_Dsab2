e681d772bb893017782036073141cc8b2df033d7
srirammurugesan <sri2014ram@gmail.com>
2026-07-23 12:27:41 +0530

    day1

---
+++ main.py
@@ -0,0 +1,28 @@
+# print("sriram\n"*3)
+# # expected output
+# """
+# sriram
+# sriram
+# sriram
+# but one print statement"""
+# print (r"C:\new\test")
+
+# write code to find even and odd numbers
+# dict={
+# "name":"sriram",
+# "gender":"male",
+# "age":20,
+# "courses":["python","java","datascience"]
+# }
+# dict["name"]="Sri"
+# dict.update({"name":["sriram","Sri"]})
+# print(dict)
+# print(dict.keys())
+# print(dict.items())
+# print(dict.values())
+# print(dict["age"])
+# print(dict.get("name"))
+l=[1,2,3,4,5,3]
+l.remove(3)
+l.pop(4)
+l.append(6)
